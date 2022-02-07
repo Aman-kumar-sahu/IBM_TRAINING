2fe67c2dd990a1fcd832269ef2c0cfe8b7b58b0c
Aman sahu <64634761+Aman-kumar-sahu@users.noreply.github.com>
2022-02-07 09:45:23 +0530

    Add files via upload

---
+++ Training/day9/yrsno/src/index.ts
@@ -0,0 +1,30 @@
+//string,number,array,object,etc
+// let s: string = "hi";
+// let s1 = "how are you";
+// console.log(`${s},${s1} MR.${4 + 5}`);
+// // console.log(s+","+s1)
+// let arr: (number | string)[];
+// arr = ["hi", 3, "hii", 4];
+// console.log(arr);
+// let emp:object;
+// let emp{
+//   fName:"tom",
+//   lName:"jerry",
+//   age:40
+// };
+// console.log(emp);
+// let stud: object;
+//  let stud : {
+//   fname: string;
+//   lname: string;
+//   age: number;
+/// };
+// let stud: object = {
+//   fname: "tom",
+//   lname: "jerry",
+//   age: 30
+// };
+// console.log(stud);
+// let data: any;
+// data = ["ji", 39, 93, "hdgkh", true];
+// console.log(data);
